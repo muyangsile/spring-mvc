9060eea8362d7e6910fd8a9048117a0b8c2ba6a7
哈喽沃德 <muyansiel@yeah.net>
2023-05-10 12:02:28 +0800

    耻辱下播--404资源未找到

---
+++ spring-mvc-01/src/main/java/com/hzlx/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.hzlx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description:
+ *
+ * @author: Mr、哈喽沃德
+ * @Date: 2023/5/10 11:59
+ * Created with IntelliJ IDEA.
+ * To change this template use File | Settings | File And Code Templates.
+ */
+@Controller
+public class TestController {
+    /**
+     * 测试方法
+     * @return
+     */
+    @RequestMapping("/test")
+    public String test(){
+        return "Hello SpringMVC";
+    }
+}
